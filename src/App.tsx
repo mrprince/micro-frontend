e63fb12e8ecf97feac713ab76a494c2850f580da
chengq4 <chengq4@lenovo.com>
2024-08-01 14:14:29 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,40 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css"
 
 function App() {
+
+    const plugins = [
+        {
+            // 在子应用所有的js之前
+            jsBeforeLoaders: [
+                {
+                    callback(appWindow: any) {
+                        console.log("js-before-loader-callback", appWindow.__WUJIE.id);
+                    }
+                }
+            ]
+        }
+    ];
+
     return (
         <>
-            <WujieReact
-                name="sub-app"
-                url="//localhost:5174/"
-            ></WujieReact>
-            <WujieReact
-                name="sub-app-1"
-                url="//localhost:5174/"
-            ></WujieReact>
+            <>home page</>
+            <div style={{display: 'fixed'}}>
+                <div>
+                    <WujieReact
+                        name="sub-app"
+                        url="//localhost:5174/"
+                        plugins={plugins}
+                    ></WujieReact>
+
+                </div>
+                <div>
+                    <WujieReact
+                        name="sub-app-1"
+                        url="//localhost:5174/"
+                        plugins={plugins}
+                    ></WujieReact>
+                </div>
+            </div>
         </>
     )
 }
